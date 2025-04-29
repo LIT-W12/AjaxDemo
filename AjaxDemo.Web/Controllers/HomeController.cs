465e4d23fddda60da1d782f141ad974a3cd2d37b
Avrumi Friedman <a.friedman07@gmail.com>
2025-04-28 21:49:17 -0400

    Add project files.

---
+++ AjaxDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AjaxDemo.Data;
+using AjaxDemo.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AjaxDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _connectionString =
+            "Data Source=.\\sqlexpress;Initial Catalog=DoctorsOffice;Integrated Security=True;Trust Server Certificate=true;";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetPeople()
+        {
+            //Thread.Sleep(2000);
+            var repo = new PeopleRepo(_connectionString);
+            return Json(repo.GetAll());
+        }
+
+        [HttpPost]
+        public void AddPerson(Person person)
+        {
+            var repo = new PeopleRepo(_connectionString);
+            repo.Add(person);
+        }
+    }
+}
---
+++ AjaxDemo.Web/wwwroot/js/people.js
@@ -0,0 +1,52 @@
+﻿$(() => {
+
+    const modal = new bootstrap.Modal($(".modal")[0]);
+
+    const refreshPeople = (cb) => {
+        $("tbody tr:gt(0)").remove();
+        $("#spinner-row").show();
+        $.get('/home/getpeople', function (people) {
+            $("#spinner-row").hide();
+            people.forEach(person => {
+                $("tbody").append(`<tr>
+                <td>${person.firstName}</td>
+                <td>${person.lastName}</td>
+                <td>${person.age}</td>
+                </tr>`);
+            })
+            if (cb) {
+                cb();
+            }
+        });
+
+    }
+
+    $("#show-add").on('click', function () {
+        $("#firstName").val('');
+        $("#lastName").val('');
+        $("#age").val('');
+        modal.show();
+    })
+
+    $("#save-person").on('click', function () {
+        const firstName = $("#firstName").val();
+        const lastName = $("#lastName").val();
+        const age = $("#age").val();
+
+        $.post('/home/addperson', {
+            firstName: firstName,
+            lastName: lastName,
+            age: age
+        }, function () {
+            refreshPeople();
+            modal.hide();
+            
+        });
+
+        
+
+       
+    })
+
+    refreshPeople();
+})
